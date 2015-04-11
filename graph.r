66adb9e84f15c072b80c9ed4660802df8f44e9bc
Brady Turner <bturner@nuvotechnologies.com>
2015-04-11 16:32:12 -0400

    graph labels

--- graph.r
+++ graph.r
@@ -24,7 +24,8 @@ p <- ggplot(d2.m, aes(timestamp,value, color=variable)) +
       geom_line() +
       ggtitle("Latency Test Results") +
       ylab("Latency (s)") +
-      xlab("Time (s)")
+      xlab("Time (s)") +
+      labs(color = "Measurement")
 # Plot as pdf
 plot(p)
 
@@ -34,10 +35,32 @@ d3$timestamp <- NULL
 
 # individual graphs of each dataset
 for (i in names(d3)) {
-  p4 <- ggplot(data, aes(x=timestamp, y=data[[i]])) + geom_line()
-  plot(p4)
+  p4 <- ggplot(data, aes(x=timestamp, y=data[[i]])) +
+          geom_line() +
+          geom_point() +
+          ggtitle(i) +
+          ylab("Latency (s)") +
+          xlab("Time (s)")
+          plot(p4)
 }
 
-p2 <- ggplot(data, aes(x=timestamp, play.hw.0Src)) + geom_line()
-p3 <- ggplot(data, aes(x=timestamp, play.hw.0)) + geom_line()
+# Two plots on same page using multiplot
+p2 <- ggplot(data, aes(x=timestamp, play.hw.0Src)) +
+        geom_line() +
+        ggtitle("play.hw.0Src") +
+        ylab("Latency (s)") +
+        xlab("Time (s)")
+p3 <- ggplot(data, aes(x=timestamp, play.hw.0)) +
+        geom_line() +
+        ggtitle("play.hw.0") +
+        ylab("Latency (s)") +
+        xlab("Time (s)")
 multiplot(p2,p3)
+
+# All datasets plotted using facet_wrap
+p5 <- ggplot(d2.m, aes(timestamp,value)) +
+        geom_line() +
+        ylab("Latency (s)") +
+        xlab("Time (s)") +
+        facet_wrap(~variable, scales="free", ncol=1)
+print(p5)
