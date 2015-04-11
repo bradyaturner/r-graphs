77c1414095d96b9e5e3b62a5a25b74aa1f48e928
Brady Turner <bturner@nuvotechnologies.com>
2015-04-11 18:16:03 -0400

    table class

--- stats.rb
+++ stats.rb
@@ -2,22 +2,28 @@
 
 require 'csv'
 
+class Table
+  attr_reader :titles, :column_data
+  def initialize(csv)
+    @column_data = []
+    rows = csv.split("\n")
+    @titles = rows[0].split(",")
+    rows[1..-1].each_with_index do |row,i|
+      cd = row.split(",")
+      cd.each_with_index do |c,i2|
+        @column_data[i2] = [] unless @column_data[i2]
+        @column_data[i2] << c
+      end
+    end
+  end
+end
+
 csv = ""
 while STDIN.gets
   csv << $_
 end
 puts csv
 
-col_data = []
-
-rows = csv.split("\n")
-
-rows.each_with_index do |row,i|
-  cd = row.split(",")
-  cd.each_with_index do |c,i2|
-    col_data[i2] = [] unless col_data[i2]
-    col_data[i2] << c
-  end
-end
-
-col_data.each {|c| puts c.inspect}
+t = Table.new(csv)
+puts t.titles.inspect
+t.column_data.each {|c| puts c.inspect }
