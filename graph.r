82151df3690e04495d5d61f6ca46665da3570bae
Brady Turner <bturner@nuvotechnologies.com>
2015-04-11 13:39:42 -0400

    graph multiple lines

--- graph.r
+++ graph.r
@@ -1,10 +1,22 @@
 #!/usr/bin/env Rscript
 
 library(ggplot2)
+library(reshape2)
 
 data <- read.csv("stdin")
 
-# do this for each column except date and time
-a <- ggplot( data, aes(x=timestamp, y=play.hw.0)) + geom_line()
+# make a copy of the data
+d2 <- data
 
-plot(a)
+# get rid of date column
+d2$date <- NULL
+
+# get rid of record column TEMPORARY
+d2$rec.hw.0 <- NULL
+
+#Melt into long format with first column as the id variable
+d2.m <- melt(d2, id.vars = 1)
+
+p <- ggplot(d2.m, aes(timestamp,value, colour=variable)) + geom_line()
+
+plot(p)
